7ffa21149b3db77c79968c1bc39d6e5b10be9f4f
MatheusSilvaSouza3 <matheussilvasouza3333@gmail.com>
2022-07-28 15:19:48 -0300

    req cadastro

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@ const express = require("express");
 const app = express();
 const User = require('./models/User')
 const cors = require('cors');
-const data = []
 
 app.use(express.json());
 app.use(cors())
 
-app.post("/cadastrar", async (req, res) => {
-    await Promise.all( req.body.forEach((element, index) => {
-        data[index] = req.body[index]
-        User.create(element)
-    }))
-    .then(() => {
+app.post("/cadastrar", async function (req, res) {
+    const data = req.body
+    if (!data) {
+        console.log("Produtos não encontrados!");
+        return res.status(400).send({ error: true, message: "Produtos não encontrados" });
+    }
+    const promise = data.map((user) => { return User.create(user) })
+    try {
+        await Promise.all(promise)
         return res.json({
-            mensagen: "Usuário cadastrado com sucesso!"
-        });
-    })
-    .catch(() => {
-        return res.status(400).json({
-            error: true,
-            mensagen: "Erro: Usuário não cadastrado!"
+            mensagen: "Usuário cadastrado com sucesso!",
+            users: data
         })
-    })
-    // console.log(data)
+    } catch (error) {
+        return res.status(400).json({
+            mensagen: "Erro: Usuário não cadastrado!",
+            error,
+        })
+    }
 })
+
 
 
 app.listen(8080)
--- src/models/dp.js
+++ src/models/dp.js
@@ -1,15 +1,15 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('meubanco','root','', {
-    host: 'localhost',
-    dialect:'mysql'
+const sequelize = new Sequelize('meubanco', 'root', '', {
+     host: 'localhost',
+     dialect: 'mysql'
 });
 
 sequelize.authenticate()
-.then(function() {
-    return console.log("Conectou no Banco")
-}).catch(function() {
-    return console.log("Não conectou no Banco")
-})
+     .then(function () {
+          console.log("Conectou no Banco")
+     }).catch(function () {
+          console.log("Não conectou no Banco")
+     })
 
 module.exports = sequelize;
